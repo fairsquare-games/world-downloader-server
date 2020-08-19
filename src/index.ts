ef9963022af6167ffcddf4dbb88f63d74245f105
McJeffr <mcjeffr@gmail.com>
2020-08-19 13:38:36 +0200

    Added automatic migration for when a CERT file isn't yet available, though was defined.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { promises as fs } from "fs";
+import fss, { promises as fs } from "fs";
 import http from "http";
 import https from "https";
+import { createHttpTerminator, HttpTerminator } from "http-terminator";
 import config from "./modules/secret-config";
 import { createWorldDirectory, getWorlds, deleteWorld } from "./world-util";
 import Router from "./router";
@@ -32,29 +33,73 @@ const setCachePurger = () => {
 };
 
 /* Start server */
-if (process.env.CERT_NAME) {
-    const start = async () => {
-        await createWorldDirectory();
-        const key = fs.readFile(`certs/${process.env.CERT_NAME}.key`, "utf8");
-        const cert = fs.readFile(`certs/${process.env.CERT_NAME}.crt`, "utf8");
-        Promise.all([key, cert])
-            .then((results) => {
-                const credentials = { key: results[0], cert: results[1] };
-
-                http.createServer(app).listen(80);
-                https.createServer(credentials, app).listen(443);
-                setCachePurger();
-            })
-            .then(() => cleanup())
-            .then(() => console.log("Ready for HTTP on port 80, ready for HTTPS on port 443"))
-            .catch((err) => console.error(err));
-    };
-    start();
-} else {
-    app.listen(80, async () => {
-        console.log("Ready on port 80");
-        await createWorldDirectory();
-        await cleanup();
-        setCachePurger();
+let httpTerminator: HttpTerminator, httpsTerminator: HttpTerminator;
+const stopServers = async () => {
+    if (httpTerminator) {
+        await httpTerminator.terminate();
+    }
+    if (httpsTerminator) {
+        await httpsTerminator.terminate();
+    }
+};
+const startHttp = async () => {
+    createHttpTerminator({
+        server: http.createServer(app),
     });
+    await createWorldDirectory();
+    await cleanup();
+    setCachePurger();
+    console.log("Ready on port 80");
+};
+const startHttps = async () => {
+    await createWorldDirectory();
+
+    /* Retrieve the key and cert files */
+    const key = fs.readFile(`certs/${process.env.CERT_NAME}.key`, "utf8");
+    const cert = fs.readFile(`certs/${process.env.CERT_NAME}.crt`, "utf8");
+
+    /* Retrieve both files and start the HTTP and HTTPS servers if they exist, retry otherwise */
+    Promise.all([key, cert])
+        .then((results) => {
+            const credentials = { key: results[0], cert: results[1] };
+
+            httpTerminator = createHttpTerminator({ server: http.createServer(app).listen(80) });
+            httpsTerminator = createHttpTerminator({ server: https.createServer(credentials, app).listen(443) });
+            setCachePurger();
+        })
+        .then(() => cleanup())
+        .then(() => console.log("Ready for HTTP on port 80, ready for HTTPS on port 443"))
+        .catch((err) => {
+            if (err.code == "ENOENT") {
+                console.log(
+                    "Could not locate CERT files, starting with HTTP instead. Will attempt to migrate to HTTPS in 30 seconds..."
+                );
+                startHttp();
+                setTimeout(async () => {
+                    await stopServers();
+                    await startHttps();
+                }, 30 * 1000);
+            }
+        });
+};
+
+if (process.env.CERT_NAME) {
+    // /* Listeners for when the cert files change / get updated automatically */
+    // const keyFile = `certs/${process.env.CERT_NAME}.key`;
+    // const certFile = `certs/${process.env.CERT_NAME}.crt`;
+    // let filesUpdated = true;
+    // fss.watchFile(keyFile, async () => {
+    //     filesUpdated = true;
+    // });
+    // fss.watchFile(certFile, async () => {
+    //     filesUpdated = true;
+    // });
+
+    /* Start HTTPS */
+    // if (filesUpdated) {
+        startHttps();
+    // }
+} else {
+    /* Start HTTP */
+    startHttp();
 }
